982bbeb79929e8e9d162dc605936ff1a4570a10e
kaushlendra <jskaushal2@gmail.com>
2025-07-20 11:21:40 +0530

    comparator and quick sort

---
+++ src/main/java/scaler/sorting/QuickSort.java
@@ -0,0 +1,23 @@
+package scaler.sorting;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] A = {1, 4, 10, 2, 1, 5};
+        quicksort(A, 0,A.length-1);
+        for(int i : A)
+        {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void quicksort(int[] A, int s, int e)
+    {
+        if(s >= e)
+            return;
+
+        int pivotIndex = PartitionIndex.helper(A, s,e);
+        quicksort(A, s, pivotIndex-1);
+        quicksort(A, pivotIndex+1, e);
+    }
+
+}
