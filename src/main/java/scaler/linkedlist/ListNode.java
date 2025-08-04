f8c8fd5bb54d795dff4239b0fb36e285d391f1ee
kaushlendra <jskaushal2@gmail.com>
2025-08-04 21:12:57 +0530

    linked list

--- src/main/java/scaler/linkedlist/ListNode.java
+++ src/main/java/scaler/linkedlist/ListNode.java
@@ -1,5 +1,6 @@
 package scaler.linkedlist;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class ListNode {
@@ -9,6 +10,10 @@ public class ListNode {
     ListNode(int data){
         this.val = data;
         this.next = null;
+    }
+
+    public static ListNode createNode(int val){
+        return new ListNode(val);
     }
 
     public static ListNode createLinkedList(){
@@ -22,9 +27,11 @@ public class ListNode {
         while (value != -1) {
             System.out.println("Enter next element or -1 to end : ");
             value = scanner.nextInt();
-            ll = new ListNode(value);
-            temp.next = ll;
-            temp = ll;
+            if(value != -1){
+                ll = new ListNode(value);
+                temp.next = ll;
+                temp = ll;
+            }
         }
         return head;
     }
@@ -38,4 +45,38 @@ public class ListNode {
             temp = temp.next;
         }
     }
+
+    public static void insertNodeAtK(int value , ListNode prev){
+        ListNode newNode = new ListNode(value);
+        newNode.next = prev.next;
+        prev.next = newNode;
+    }
+
+    public static ListNode getKthNode(ListNode A,  int k)
+    {
+        ListNode temp = A; // store A into temp so that head is not misplaced
+        int i  = 0; // index = 0
+        while( i < k && temp != null) // if only one ele, then
+        {
+            temp = temp.next;
+            i++;
+        }
+
+        return temp;
+    }
+//
+//    public static ListNode deleteNode(ListNode head, int deleteValue){
+//        ListNode temp = head;
+//        if (temp == null)
+//            return temp;
+//
+//        if(temp.val == deleteValue){
+//            head = head.next;
+//            return head;
+//        }
+//
+//        while(temp.next != null){
+//
+//        }
+//    }
 }
