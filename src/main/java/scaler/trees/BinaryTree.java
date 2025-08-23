4aa8d5e00e650b9d67bd10d2ac69aa62eff395a9
kaushlendra <jskaushal2@gmail.com>
2025-08-23 11:16:21 +0530

    iterative preorder

--- src/main/java/scaler/trees/BinaryTree.java
+++ src/main/java/scaler/trees/BinaryTree.java
@@ -18,7 +18,30 @@ public class BinaryTree {
         Node(int value)
         {
             this.value = value;
+        }
 
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
         }
     }
 
