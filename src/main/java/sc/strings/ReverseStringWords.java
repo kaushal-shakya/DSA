57ac055f490eb73ff4e6b6ca3b773a931f93d1a4
kaushaldsa <jskaushal2@gmail.com>
2025-12-30 05:21:53 +0530

    reverse string words
    Signed-off-by: kaushaldsa <jskaushal2@gmail.com>

--- src/main/java/sc/strings/ReverseStringWords.java
+++ src/main/java/sc/strings/ReverseStringWords.java
@@ -1,9 +1,30 @@
 package sc.strings;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ReverseStringWords {
     public static void main(String[] args) {
         String s = "Shahrukh is the best actor";
+        System.out.println(solve(s));
     }
 
+    public static String solve(String A) {
+        A = A.trim();
+        String[] s = A.split(" ");
 
+        int l = 0;
+        int r = s.length-1;
+        while(l < r)
+        {
+            String temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }
+
+        List<String> reversedArray = Arrays.asList(s);
+        return String.join(" ", reversedArray);
+    }
 }
