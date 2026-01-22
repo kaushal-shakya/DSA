768c258f338d358e31d0dc1001e7394cfb486484
kaushaldsa <jskaushal2@gmail.com>
2026-01-22 17:04:24 +0530

    unique bst count
    Signed-off-by: kaushaldsa <jskaushal2@gmail.com>

---
+++ src/main/java/sc/dp/UniqueBST.java
@@ -0,0 +1,21 @@
+package sc.dp;
+
+public class UniqueBST {
+    public static void main(String[] args) {
+        System.out.println(numTrees(5));
+    }
+
+    public static int numTrees(int A) {
+        int[] dp = new int[A+1];
+        dp[0] = 1; dp[1] = 1;
+
+        for(int i=2; i <= A; i++) {
+            int ans = 0;
+            for(int j=1; j<=i; j++) {
+                ans += dp[j-1]* dp[i-j];
+            }
+            dp[i] = ans;
+        }
+        return dp[dp.length-1];
+    }
+}
