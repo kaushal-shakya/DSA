69a6171a4f297e75a0b660572af9c46d594d3169
kaushaldsa <jskaushal2@gmail.com>
2026-01-25 01:12:45 +0530

    knapsack
    Signed-off-by: kaushaldsa <jskaushal2@gmail.com>

---
+++ src/main/java/sc/dp/FractionalKnapsack.java
@@ -0,0 +1,80 @@
+package sc.dp;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Pair {
+    int happiness;
+    int weight;
+
+    public Pair(int happiness, int weight) {
+        this.happiness = happiness;
+        this.weight = weight;
+    }
+}
+
+class FractionKnapsackIntermediate {
+
+    private BigDecimal helper(Pair[] p, int capacity) {
+        BigDecimal ans = BigDecimal.ZERO;
+        for (Pair item : p) {
+            if(item.weight <= capacity) {
+                ans = ans.add(BigDecimal.valueOf(item.happiness));
+                capacity -= item.weight;
+            } else {
+                BigDecimal fraction = BigDecimal.valueOf(capacity)
+                        .divide(BigDecimal.valueOf(item.weight), 10, RoundingMode.HALF_UP);
+                ans = ans.add(BigDecimal.valueOf(item.happiness).multiply(fraction));
+                break;
+            }
+        }
+        return ans;
+    }
+
+    int solve(int[] happiness, int[] weight, int capacity)
+    {
+        Pair[] pair = new Pair[happiness.length];
+        for (int i = 0; i < happiness.length; i++) {
+            Pair p = new Pair(happiness[i], weight[i]);
+            pair[i] = p;
+        }
+
+        Arrays.sort(pair, new Comparator<Pair>() {
+            @Override
+            public int compare(Pair o1, Pair o2) {
+                double o1HappinessPerWeight = (o1.happiness * 1.0 / o1.weight);
+                double o2HappinessPerWeight = (o2.happiness * 1.0 / o2.weight);
+
+                if(o1HappinessPerWeight < o2HappinessPerWeight)
+                    return 1;
+                else if ( o1HappinessPerWeight > o2HappinessPerWeight)
+                    return -1;
+                else
+                    return 0;
+            }
+        });
+
+        BigDecimal result = helper(pair, capacity).multiply(BigDecimal.valueOf(100));
+        return result.setScale(0, RoundingMode.FLOOR).intValue();
+    }
+}
+
+
+public class FractionalKnapsack {
+    public static void main(String[] args) {
+        int[] happiness = {16,3,3,6,7,8,17,13,7};
+        int[] weight = {3,10,9,18,17,17,6,16,13};
+        int capacity = 11;
+//        int[] happiness = {3};
+//        int[] weight = {20};
+//        int capacity = 17;
+//        int[] happiness = {2,7};
+//        int[] weight = {11,3};
+//        int capacity = 7;
+
+        FractionKnapsackIntermediate fki = new FractionKnapsackIntermediate();
+        System.out.println(fki.solve(happiness, weight, capacity));
+    }
+}
---
+++ src/main/java/sc/dp/zero_one_knapsack.java
@@ -0,0 +1,70 @@
+package sc.dp;
+
+import java.util.Arrays;
+
+class ZeroOneKnapsack {
+
+    private int helper_tabulation(int[] A, int[] B, int capacity) {
+        int[][] dp = new int[A.length+1][capacity+1];
+
+        for (int i = 1; i <= A.length; i++) {
+            for (int j = 1; j <= capacity; j++) {
+                int rej = dp[i-1][j];
+                int sel = 0;
+                int w = B[i-1];
+                int v = A[i-1];
+                if(w <= j)
+                    sel  = dp[i-1][j-w] + v;
+                dp[i][j] = Math.max(rej, sel);
+            }
+        }
+        return dp[A.length][capacity];
+    }
+
+    private int helper_memoization(int i, int j, int[] A, int[] B, int[][] dp) {
+        if(i < 0 || j == 0)
+            return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+        int reject = helper_memoization(i-1, j, A, B, dp);
+        int pick = 0;
+        if(B[i] <= j)
+            pick = helper_memoization(i-1, j-B[i], A, B, dp) + A[i];
+
+        return dp[i][j] = Math.max(reject, pick);
+    }
+
+    private int helper_recursive(int i, int j, int[] A, int[] B) {
+
+        if(i < 0 || j == 0)
+            return 0;
+        int reject = helper_recursive(i-1, j, A, B);
+        int pick = 0;
+        if(B[i] <= j)
+            pick = helper_recursive(i-1, j-B[i], A, B) + A[i];
+
+        return Math.max(reject, pick);
+    }
+
+    public int solve(int[] A, int[] B, int C) {
+        int[][] dp = new int[A.length+1][C+1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+//        return helper_recursive(A.length-1, C, A, B);
+//        return helper_memoization(A.length-1, C, A, B, dp);
+        return helper_tabulation(A, B, C);
+    }
+}
+
+
+public class zero_one_knapsack {
+    public static void main(String[] args) {
+
+        int[] value = {60, 100, 120};
+        int[] weight = {10, 20, 30};
+        int capacity = 50;
+        ZeroOneKnapsack zok = new ZeroOneKnapsack();
+        System.out.println(zok.solve(value, weight, capacity));
+    }
+}
