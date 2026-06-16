c4d539f991268191fc5c178bcc8cc0d82f480d57
kaushaldsa <jskaushal2@gmail.com>
2026-06-16 07:22:41 +0530

    prototype
    Signed-off-by: kaushaldsa <jskaushal2@gmail.com>

--- src/main/java/lld2/prototype/game/v3/ClientPrototype.java
+++ src/main/java/lld2/prototype/game/v3/ClientPrototype.java
@@ -38,6 +38,23 @@ public class ClientPrototype {
     public static void main(String[] args) {
         fillRegistry();
 
+
+        /**
+         * Hides complexities from the client - If you didn't have the Prototype Pattern, the developer writing main would have had to write 9 lines of code (calling setMap, setGravity, setWeaponsEnabled, etc.) just to create the asiaMatch.
+         * Furthermore, they would have had to memorize exactly what the Erangel defaults are supposed to be.
+         * Because of the Prototype Pattern, all that messy configuration is hidden away in fillRegistry.
+         */
+        GameConfig gameConfig1 = gameConfigRegistry.get("Asia-Erangel").clone();
+        gameConfig1.setWeatherType("Cloudy");
+        gameConfig1.setServerRegion("Africa");
+
+        /** Generates objects of unknown types to client
+         Registry stores everything as the base GameConfig type.
+         The client code thinks it is just dealing with a standard GameConfig.
+         It doesn't actually know that it is secretly cloning a RankedGameConfig.
+         Because of polymorphism, your .clone() method automatically executes the correct logic for the ranked subclass.
+         You successfully duplicated a specific subclass without the client ever needing to use new RankedGameConfig()
+         or write instanceof checks. */
         GameConfig gameConfig = gameConfigRegistry.get("Ranked-Erangel").clone();
         gameConfig.setWeatherType("Clear");
         gameConfig.setServerRegion("US");
