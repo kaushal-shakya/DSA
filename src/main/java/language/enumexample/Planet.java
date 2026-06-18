bf2a34d6f89894fed49701c201692bce5b612d70
kaushaldsa <jskaushal2@gmail.com>
2026-06-18 20:18:22 +0530

    enum
    Signed-off-by: kaushaldsa <jskaushal2@gmail.com>

---
+++ src/main/java/language/enumexample/Planet.java
@@ -0,0 +1,52 @@
+package language.enumexample;
+
+public enum Planet {
+
+    //These values are passed to the constructor when the constant is created.
+    // Java requires that the constants be defined first, prior to any fields or methods.
+    MERCURY (3.303e+23, 2.4397e6),
+    VENUS   (4.869e+24, 6.0518e6),
+    EARTH   (5.976e+24, 6.37814e6),
+    MARS    (6.421e+23, 3.3972e6),
+    JUPITER (1.9e+27,   7.1492e7),
+    SATURN  (5.688e+26, 6.0268e7),
+    URANUS  (8.686e+25, 2.5559e7),
+    NEPTUNE (1.024e+26, 2.4746e7);
+
+    // Enum properties
+    private final double mass;   // in kilograms
+    private final double radius; // in meters
+
+    //The constructor for an enum type must be default or private access.
+    // It automatically creates the constants that are defined at the beginning of the enum body.
+    // You cannot invoke an enum constructor yourself.
+    Planet(double mass, double radius) {
+        this.mass = mass;
+        this.radius = radius;
+    }
+
+    // Enum methods
+    private double mass() { return mass; }
+    private double radius() { return radius; }
+
+    // universal gravitational constant  (m3 kg-1 s-2)
+    public static final double G = 6.67300E-11;
+
+    double surfaceGravity() {
+        return G * mass / (radius * radius);
+    }
+    double surfaceWeight(double otherMass) {
+        return otherMass * surfaceGravity();
+    }
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Usage: java Planet <earth_weight>");
+            System.exit(-1);
+        }
+        double earthWeight = Double.parseDouble(args[0]);
+        double mass = earthWeight/EARTH.surfaceGravity();
+        for (Planet p : Planet.values())
+            System.out.printf("Your weight on %s is %f%n",
+                    p, p.surfaceWeight(mass));
+    }
+}
---
+++ src/main/java/lld3/pen/Pen.java
@@ -0,0 +1,7 @@
+package lld3.pen;
+
+public abstract class Pen {
+    String name;
+    String brand;
+    double price;
+}
