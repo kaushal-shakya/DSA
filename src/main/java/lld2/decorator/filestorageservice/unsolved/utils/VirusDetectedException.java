2c51e156fefbf5fb45b4cbb93e6a4e26bf484578
kaushaldsa <jskaushal2@gmail.com>
2026-06-21 19:34:24 +0530

    decorator
    Signed-off-by: kaushaldsa <jskaushal2@gmail.com>

---
+++ src/main/java/lld2/decorator/filestorageservice/unsolved/utils/VirusDetectedException.java
@@ -0,0 +1,8 @@
+package lld2.decorator.filestorageservice.unsolved.utils;
+
+public class VirusDetectedException extends RuntimeException {
+    public VirusDetectedException(String message) {
+        super(message);
+    }
+
+}
