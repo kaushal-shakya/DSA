d05d2dc36c1f57b677b7df14df14a32123e7928a
kaushaldsa <jskaushal2@gmail.com>
2026-06-21 19:28:31 +0530

    decorator
    Signed-off-by: kaushaldsa <jskaushal2@gmail.com>

--- src/main/java/lld2/decorator/imageediting/unsolved/RawImageEditor.java
+++ src/main/java/lld2/decorator/imageediting/unsolved/RawImageEditor.java
@@ -1,7 +1,5 @@
 package lld2.decorator.imageediting.unsolved;
 
-import decorator.utils.Image;
-
 /**
  * TODO: Task 2 - Implement the ImageEditor interface to simply reads and return the image without any modifications.
  * Methods you can use
